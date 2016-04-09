c14716f022ebe7f463ed6150a0724e600db27fcd
Thomas Adriano <thomas@Thomass-MacBook-Pro.local>
2016-04-08 21:09:34 -0300

    commit inicial

---
+++ N3_1/src/main/java/br/furb/Frame.java
@@ -0,0 +1,42 @@
+package br.furb;
+
+import java.awt.BorderLayout;
+
+import javax.media.opengl.GLCapabilities;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+
+
+public class Frame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7541728312795865667L;
+	public static final int X = 400;
+	public static final int Y = 400;
+	public static final int LARGURA_BORDA_JANELA_WINDOWS = 22;
+	public static final int LARGURA_JANELA = X;
+	public static final int ALTURA_JANELA = Y;
+	
+	public Frame() {
+		super("N3_1");
+		setBounds(X, Y, LARGURA_JANELA, ALTURA_JANELA + LARGURA_BORDA_JANELA_WINDOWS);
+	
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new BorderLayout());
+		
+		
+		/* Cria um objeto GLCapabilities para especificar 
+		 * o numero de bits por pixel para RGBA
+		 */
+		GLCapabilities glCaps = new GLCapabilities();
+		glCaps.setRedBits(8);
+		glCaps.setBlueBits(8);
+		glCaps.setGreenBits(8);
+		glCaps.setAlphaBits(8); 
+
+	}
+	
+}
